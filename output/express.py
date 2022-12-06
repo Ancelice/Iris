27c5d5be4bd2cffab4db0fbe69825979ae4295b1
ANLI <103622417+Ancelice@users.noreply.github.com>
2022-12-06 17:21:29 +0900

    Delete output directory

--- output/express.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import plotly.express as px
-covid_df = pd.read_csv('./sample_data/owid-covid-data.csv',index_col='date',parse_dates=True)
-fig = px.choropleth(covid_df, 
-                    locations="iso_code",
-                    color='total_cases', 
-                    animation_frame=covid_df.index.strftime('%Y/%m/%d'),
-                    range_color=[0,1000000])
-fig.show()
